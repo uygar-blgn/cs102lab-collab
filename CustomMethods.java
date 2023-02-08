40c2afbddf29e2dcf275744400ab731e9289ff3f
uygar-blgn <uygarblgn13@outlook.com>
2023-02-08 10:00:57 +0300

    did the c

--- CustomMethods.java
+++ CustomMethods.java
@@ -1,3 +1,22 @@
 public class CustomMethods {
     
+    public static int findMin(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
