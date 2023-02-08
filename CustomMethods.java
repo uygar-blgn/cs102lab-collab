b6c68d29d4d6929e128ddf2935030a548b1d0b05
uygar-blgn <uygarblgn13@outlook.com>
2023-02-08 10:26:08 +0300

    Moved ArrayOps to CustomMethods

--- CustomMethods.java
+++ CustomMethods.java
@@ -61,4 +61,25 @@ public class CustomMethods {
         return array;
      
     }
+
+    public static int arrayAv(int[] array){
+        int total = Integer.MIN_VALUE;
+
+        for(int i = 0; i < array.length; i++) {
+            total += i;
+        }
+
+        return total/array.length;
+    }
+    
+    public static Integer[] diffAverageF(int[] array){
+        Integer[] diffAverage = new Integer[array.length];
+
+        for (int i = 0; i < array.length; i++){
+            diffAverage[i] = array[i] - arrayAv(array);
+        }
+
+        return diffAverage;
+
+    }
 }
