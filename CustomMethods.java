9ff091cd77f4d392be15285de35a216ad8f02e10
Ahmet Zor <ahmetzor2003xx3@gmail.com>
2023-02-08 10:13:56 +0300

    added even and odd summation

--- CustomMethods.java
+++ CustomMethods.java
@@ -19,4 +19,20 @@ public class CustomMethods {
         }
         return max;
     }
+
+    public static int findEven(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i = i + 2){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static int findOdd(int[] arr){
+        int sum = 0;
+        for (int i = 1; i < arr.length; i = i + 2){
+            sum += arr[i];
+        }
+        return sum;
+    }
 }
